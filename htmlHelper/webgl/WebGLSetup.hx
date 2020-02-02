550f1dfcc881725679ea9c7c2fdcac750e4a01a9
nanjizal <none>
2020-02-02 10:49:18 +0000

    update with potential image/texture support.

--- htmlHelper/webgl/WebGLSetup.hx
+++ htmlHelper/webgl/WebGLSetup.hx
@@ -8,12 +8,10 @@ import js.html.webgl.ContextAttributes;
 import js.html.webgl.Shader;
 import js.html.webgl.Program;
 import js.html.webgl.UniformLocation;
-#if (haxe_ver < 4.0 )
-import js.html.Float32Array;
-#else
-import  js.lib.Float32Array;
-#end
-// import js.html.Uint16Array;
+import js.html.Image;
+import haxe.io.UInt16Array;
+import haxe.io.Float32Array;
+import haxe.io.Int32Array;
 import htmlHelper.webgl.WebGLSetup;
 using htmlHelper.webgl.WebGLSetup;
 typedef RGB = {
@@ -34,12 +32,14 @@ class WebGLSetup {
     public var DEPTH_TEST      = true;
     public var CULL_FACE       = true; 
     public var BACK            = true;
-    public var matrix32Array   = new Float32Array( ident() ); // internal matrix passed to shader
-    var vertices               = new Array<Float>();
+    public var matrix32Array   : Float32Array; 
+    var vertices               = new Float32Array(100);
     var triangleColors:        Array<UInt>;
-    var indices                = new Array<Int>();
-    var colors                 = new Array<Float>();
-    public function new( width_: Int, height_: Int, autoChild: Bool = true ){
+    var indices                = new UInt16Array(100);
+    var colors                 = new Float32Array(100);
+    public
+    function new( width_: Int, height_: Int, autoChild: Bool = true ){
+        matrix32Array     = ident(); // internal matrix passed to shader
         width             = width_;
         height            = height_;
         canvas            = Browser.document.createCanvasElement();
@@ -55,31 +55,51 @@ class WebGLSetup {
         if( autoChild ) Browser.document.body.appendChild( cast canvas );
         gl = canvas.getContextWebGL();
     }
-    public function setupProgram( vertexString: String, fragmentString: String ): Program {
-        var vertex   = gl.createVertexShader( vertexString );
-        var fragment = gl.createFragmentShader( fragmentString );
+    public
+    function setupProgram( vertexString: String, fragmentString: String ): Program {
+        var vertex   = gl.shaderFromString( RenderingContext.VERTEX_SHADER,   vertexString );
+        var fragment = gl.shaderFromString( RenderingContext.FRAGMENT_SHADER, fragmentString );
         program      = gl.createShaderProgram( vertex, fragment );
         return program;
     }
-    public function clearVerticesAndColors(){
-        vertices = new Array<Float>();
-        indices  = new Array<Int>();
-        colors   = new Array<Float>();
-    }    
-    public function setVerticesAndColors( vertices: Array<Float>, triangleColors: Array<UInt> ){
+    public
+    function addImage( img: Image ){
+        uploadImage( gl, img );
+    }
+    public
+    function clearVerticesAndColors(){
+        var vl = vectices.length;
+        var il = indices.length;
+        var cl = colors.length;
+        vertices = new Float32Array(vl);
+        indices  = new UInt16Array(il);
+        colors   = new Float32Array(cl);
+        // texture?
+    }
+    static inline
+    function passIndicesToShader( gl: RenderingContext, indices: UInt16Array ){
+        var indexBuffer = gl.createBuffer(); // triangle indicies data 
+        gl.bindBuffer( RenderingContext.ELEMENT_ARRAY_BUFFER, indexBuffer );
+        gl.bufferData( RenderingContext.ELEMENT_ARRAY_BUFFER, untyped indices, RenderingContext.STATIC_DRAW );
+        gl.bindBuffer( RenderingContext.ELEMENT_ARRAY_BUFFER, null );
+    }
+    public
+    function setVerticesAndColors( vertices: Float32Array, triangleColors: Array<UInt> ){
         var rgb: RGB;
         var colorAlpha = 1.0;
+        var len = colors.length;
         for (i in 0...Std.int( vertices.length/3 ) ) {
             rgb = toRGB( triangleColors[ i ] );
             for( j in 0...3 ){  // works but...
-                colors.push( rgb.r );
-                colors.push( rgb.g );
-                colors.push( rgb.b );
-                colors.push( colorAlpha );
+                colors[len++] = rgb.r;
+                colors[len++] = rgb.g;
+                colors[len++] = rgb.b;
+                colors[len++] = colorAlpha;
             }
             indices.push( i );
         }
-        gl.uploadDataToBuffers( program, vertices, colors, indices );
+        gl.passIndicesToShader( indicies );
+        gl.uploadDataToBuffers( program, vertices, colors );
     }
     public function render(){
         gl.clearColor( bgRed, bgGreen, bgBlue, bgAlpha );
@@ -92,22 +112,20 @@ class WebGLSetup {
         gl.viewport( 0, 0, canvas.width, canvas.height );
         var modelViewProjectionID = gl.getUniformLocation( program, 'modelViewProjection' );
         /// you can update matrix32Array in the render loop.
-        gl.uniformMatrix4fv( modelViewProjectionID, false, matrix32Array );
+        gl.uniformMatrix4fv( modelViewProjectionID, false, untyped matrix32Array );
         gl.drawArrays( RenderingContext.TRIANGLES, 0, indices.length );
     }
-    static inline function createVertexShader( gl: RenderingContext, str: String ): Shader {
-        var vertexShader = gl.createShader( RenderingContext.VERTEX_SHADER );
-        gl.shaderSource(  vertexShader, str );
-        gl.compileShader( vertexShader );
-        return vertexShader;
+    static inline
+    function shaderFromString( gl:           RenderingContext
+                             , shaderType:   Int
+                             , shaderString: String ){
+        var shader = gl.createShader( shaderType );
+        gl.shaderSource(  shader, shaderString );
+        gl.compileShader( shader );
+        return shader;
     }
-    static inline function createFragmentShader( gl: RenderingContext, str: String ): Shader {
-        var fragmentShader = gl.createShader( RenderingContext.FRAGMENT_SHADER );
-        gl.shaderSource(  fragmentShader, str ); 
-        gl.compileShader( fragmentShader );
-        return fragmentShader;
-    }
-    static inline function createShaderProgram( gl: RenderingContext, vertex: Shader, fragment: Shader ): Program {
+    static inline
+    function createShaderProgram( gl: RenderingContext, vertex: Shader, fragment: Shader ): Program {
         var program = gl.createProgram();
         gl.attachShader( program, vertex );
         gl.attachShader( program, fragment );
@@ -115,57 +133,78 @@ class WebGLSetup {
         gl.useProgram(   program );
         return program;
     }
-    
+    static inline
+    function shaderInput<T>( gl:      RenderingContext
+                                           , program: Program
+                                           , name:    String
+                                           , att:     Int
+                                           , arr:     T /*Float32Array */
+                                           , number:  Int ){
+        var buffer = gl.createBuffer();
+        var arrBuffer = RenderingContext.ARRAY_BUFFER;
+        gl.bindBuffer( arrBuffer, buffer );
+        // RenderingContext.FLOAT, RenderingContext.INT, RenderingContext.UNSIGNED_INT
+        // Float32Array,Int32Array, Uint16Array
+        gl.bufferData( arrBuffer, untyped arr, RenderingContext.STATIC_DRAW );
+        var flo = gl.getAttribLocation( program, name );
+        gl.vertexAttribPointer( flo, att, number, false, 0, 0 ); 
+        gl.enableVertexAttribArray( flo );
+        gl.bindBuffer( arrBuffer, null );
+    }
+    /*
     static inline function uploadTriangleDataToBuffers( gl: RenderingContext, program: Program, vertices: Array<Float>, indices: Array<Int> ){
-        var vertexBuffer = gl.createBuffer(); // position data
-        gl.bindBuffer( RenderingContext.ARRAY_BUFFER, vertexBuffer );
-        gl.bufferData( RenderingContext.ARRAY_BUFFER, new Float32Array( vertices ), RenderingContext.STATIC_DRAW );
-        var position = gl.getAttribLocation( program, "pos" );
-        gl.vertexAttribPointer( position, 3, RenderingContext.FLOAT, false, 0, 0 ); 
-        gl.enableVertexAttribArray( position );
-        gl.bindBuffer( RenderingContext.ARRAY_BUFFER, null );
         //var indexBuffer = gl.createBuffer(); // triangle indicies data 
         //gl.bindBuffer( RenderingContext.ELEMENT_ARRAY_BUFFER, indexBuffer );
         //gl.bufferData( RenderingContext.ELEMENT_ARRAY_BUFFER, new Uint16Array( indices ), RenderingContext.STATIC_DRAW );
         //gl.bindBuffer( RenderingContext.ELEMENT_ARRAY_BUFFER, null );
     }
-    
-    static inline function uploadDataToBuffers( gl: RenderingContext, program: Program, vertices: Array<Float>, colors: Array<Float>, indices: Array<Int> ){
-        gl.uploadTriangleDataToBuffers( program, vertices, indices );
-        gl.uploadSimpleColorToBuffers(  program, colors ); // color data
+    */
+    static inline
+    function uploadDataToBuffers( gl: RenderingContext, program: Program, vertices: Float32Array, colors: Float32Array, ?texture: Float32Array ){//, indices: Uint16Array ){
+        gl.shaderInput( program, 'pos',   3, vertices, RenderingContext.FLOAT );
+        gl.shaderInput( program, 'color', 4, colors, RenderingContext.FLOAT );
+        if( texture != null ) gl.shaderInput( program, 'aTexture', 2, texture, RenderingContext.FLOAT );
     }
-    
-    static inline function uploadFloatToBuffers( gl: RenderingContext, program: Program, name: String, att: Int, arr: Array<Float> ){
-        var floatBuffer = gl.createBuffer();
-        gl.bindBuffer( RenderingContext.ARRAY_BUFFER, floatBuffer );
-        gl.bufferData( RenderingContext.ARRAY_BUFFER, new Float32Array( arr ), RenderingContext.STATIC_DRAW );
-        var flo = gl.getAttribLocation( program, name );
-        gl.vertexAttribPointer( flo, att, RenderingContext.FLOAT, false, 0, 0 ); 
-        gl.enableVertexAttribArray( flo );
-        gl.bindBuffer( RenderingContext.ARRAY_BUFFER, null );
+    // TODO: generalize
+    static inline
+    function uploadImage( gl: RenderingContext, image: Image ){
+        var texture = gl.createTexture();
+        gl.activeTexture( RenderingContext.TEXTURE0 );
+        gl.bindTexture( RenderingContext.TEXTURE_2D, texture );
+        gl.pixelStorei( RenderingContext.UNPACK_FLIP_Y_WEBGL, 1 );
+        gl.texParameteri( RenderingContext.TEXTURE_2D
+                        , RenderingContext.TEXTURE_WRAP_S
+                        , RenderingContext.CLAMP_TO_EDGE );
+        gl.texParameteri( RenderingContext.TEXTURE_2D
+                        , RenderingContext.TEXTURE_WRAP_T
+                        , RenderingContext.CLAMP_TO_EDGE );
+        gl.texParameteri( RenderingContext.TEXTURE_2D
+                        , RenderingContext.TEXTURE_MIN_FILTER
+                        , RenderingContext.NEAREST );
+        gl.texParameteri( RenderingContext.TEXTURE_2D
+                        , RenderingContext.TEXTURE_MAG_FILTER
+                        , RenderingContext.NEAREST );
+        gl.texImage2D(    RenderingContext.TEXTURE_2D, 0
+                        , RenderingContext.RGBA
+                        , RenderingContext.RGBA
+                        , RenderingContext.UNSIGNED_BYTE
+                        , image );
     }
-    
-    static inline function uploadSimpleColorToBuffers( gl: RenderingContext, program: Program, colors: Array<Float> ){
-        var colorBuffer = gl.createBuffer();
-        gl.bindBuffer( RenderingContext.ARRAY_BUFFER, colorBuffer );
-        gl.bufferData( RenderingContext.ARRAY_BUFFER, new Float32Array( colors ), RenderingContext.STATIC_DRAW );
-        var col = gl.getAttribLocation( program, "color" );
-        gl.vertexAttribPointer( col, 4, RenderingContext.FLOAT, false, 0, 0 ); 
-        gl.enableVertexAttribArray( col );
-        gl.bindBuffer( RenderingContext.ARRAY_BUFFER, null );
-    }
-    public static inline function toRGB(int:Int) : RGB {
+    public static inline
+    function toRGB(int:Int) : RGB {
         return {
             r: ((int >> 16) & 255) / 255,
             g: ((int >> 8) & 255) / 255,
             b: (int & 255) / 255
         }
     }
-    public static inline function ident(): Array<Float> {
-        return [ 1.0, 0.0, 0.0, 0.0,
-                 0.0, 1.1, 0.0, 0.0,
-                 0.0, 0.0, 1.0, 0.0,
-                 0.0, 0.0, 0.0, 1.0
-                 ];
+    public static inline
+    function ident(): Float32Array {
+        var arr = new Float32Array(16);
+        arr[0] = 1.0; arr[1] = 0.0; arr[2] = 0.0; arr[3] = 0.0;
+        arr[4] = 1.0; arr[5] = 0.0; arr[6] = 0.0; arr[7] = 0.0;
+        arr[8] = 1.0; arr[9] = 0.0; arr[10] = 0.0; arr[11] = 0.0;
+        arr[12] = 1.0; arr[13] = 0.0; arr[14] = 0.0; arr[15] = 0.0;
+        return arr;
     }
 }
