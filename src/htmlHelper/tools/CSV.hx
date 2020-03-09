f28cc5dbb5c932ad510b1fe5b794873bc10774cf
nanjizal <nanjizal@users.noreply.github.com>
2020-03-09 01:50:39 +0000

    Create CSV.hx for parsing csv text files.

---
+++ src/htmlHelper/tools/CSV.hx
@@ -0,0 +1,37 @@
+package htmlHelper.tools;
+
+import htmlHelper.tools.StringCodeIterator;
+// untested but based on working code, so likely not far off.
+class CSV {
+    public static inline
+    function parse( str: String ): Array<Array<String>> {
+        var sl        = new StringCodeIterator( str );
+        sl.next();
+        var arr = new Array<Array<String>>();
+        var arrTemp = new Array<String>();
+        var no = 0;
+        var count = 0;
+        while( sl.hasNext() ){
+            switch( sl.c ){
+                case '\n'.code | '\r'.code:
+                    arr[ no++ ] = arrTepm.copy();
+                    count = 0;
+                    sl.resetBuffer();
+                case ','.code: // case comma
+                    switch( count ){
+                        case 0:
+                            arrTemp.length = 0;
+                            arrTemp[0] = sl.toStr();
+                        default:
+                            arrTemp[ count ] = sl.toStr();
+                      }
+                      count++;
+                      sl.resetBuffer();
+                default: // otherwise
+                      sl.addChar();
+            }
+            sl.next();
+        }
+        return arr;
+    }
+}
