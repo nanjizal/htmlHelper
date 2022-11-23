48fd0bfa6dbb60c16c036ff2d0b98a4dfd0e1e5b
nanjizal <nanjizal@users.noreply.github.com>
2022-11-23 11:25:43 +0000

    Update CanvasSetup.hx

--- src/htmlHelper/canvas/CanvasSetup.hx
+++ src/htmlHelper/canvas/CanvasSetup.hx
@@ -14,15 +14,15 @@ class CanvasSetup {
     public
     function new(){
         divertTrace = new DivertTrace();
-        var canvas = new CanvasWrapper();
-        canvas.width  = 1024;
-        canvas.height = 768;
-        var dom = cast canvas;
+        canvasWrapper = new CanvasWrapper();
+        canvasWrapper.width  = 1024;
+        canvasWrapper.height = 768;
+        var dom = cast canvasWrapper;
         var style = dom.style;
         style.background = 'black';
-        Browser.document.body.appendChild( cast canvas );
-        surface = new Surface({ x: 10, y: 10, me: canvas.getContext2d() });
-        overSampleCanvas();
+        Browser.document.body.appendChild( cast canvasWrapper );
+        surface = new Surface({ x: 10, y: 10, me: canvasWrapper.getContext2d() });
+        overSampleCanvas( canvas );
     }
     function overSampleCanvas(){
       var ctx = surface.me;
@@ -31,14 +31,16 @@ class CanvasSetup {
       var height = canvas.height;
       canvas.width = 0 | Std.int(width * factor);
       canvas.height = 0 | Std.int(height * factor);
-      canvas.style.width = width + 'px';
-      canvas.style.height = height + 'px';
+      var dom = cast canvasWrapper;
+      var style = dom.style;
+      style.width = width + 'px';
+      style.height = height + 'px';
       ctx.scale( factor, factor );
     }
     public inline
     function clear(){
         var ctx = surface.me;
-        ctx.clearRect( 0, 0, canvas.width, canvas.height );
-        overSampleCanvas( canvas, surface.me, factor );
+        ctx.clearRect( 0, 0, canvasWrapper.width, canvasWrapper.height );
+        overSampleCanvas();
     }
 }
